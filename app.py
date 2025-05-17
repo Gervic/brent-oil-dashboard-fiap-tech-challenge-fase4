5b391f70c62b5cdb4f7b122748b3e1879616dfae
Victorien Nago <ger.nago@gmail.com>
2025-05-17 14:51:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import yfinance as yf
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
